07755b7e45e7d97247647e8a93068da77dafdbd0
Ujjwal <ujjwalchoudhary065@gmail.com>
2023-07-20 17:04:46 +0530

    Updated Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -4,7 +4,7 @@ const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page(protected)</p>
-      <UserButton afterSignOutUrl="/" />
+    
     </div>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { UserButton } from "@clerk/nextjs"
+
+import MobileSidebar from "./Mobile-sidebar"
+
+
+const Navbar = () => {
+  return (
+      <div className="flex items-center p-4">
+         <MobileSidebar />
+          <div className="flex justify-end w-full">
+              <UserButton afterSignOutUrl="/" />
+          </div>
+    </div>
+    
+  )
+}
+
+export default Navbar
